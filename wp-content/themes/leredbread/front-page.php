b8778769c62496328a84b03104e4ff3fcc6824e9
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-03-08 18:05:37 -0800

    Add testimonials

--- wp-content/themes/leredbread/front-page.php
+++ wp-content/themes/leredbread/front-page.php
@@ -19,6 +19,86 @@ get_header(); ?>
 			<h1 class="hero-text">Baked to Perfection.</h1>
 		</section>
 
+		<section class="products-bait">
+		         <p>
+		            <span>All our products are made fresh daily from locally-sourced ingredients. Our menu is updated frequently.</span>
+		            <a href="/products/" class="btn">See Our Products</a>
+		         </p>
+		      </section>
+
+
+		<section class="testimonials-section">
+			<div>
+
+					<h2>What others say about us</h2>
+
+				</div>
+
+				<div class="testimonial-list">
+
+						<div class="testimonial-one indiv-testimonial">
+
+							<div>
+								<img src="wp-content/uploads/2013/03/dr-dosist-headshot.png" alt="dr-dosist-headshot" width="607" height="608" class="alignnone size-full wp-image-1458" />
+							</div>
+
+							<div class="testimonial-text">
+								<p>Healthy food be damned! If you are a bakery and sweet addict like myself this place is life changing.
+									So many goodies. Have a cheat day and eat all the pretzels!<p>
+								<h3>Dr. Dosist</h3>
+								<p>Nutritionist - <span>Health Time Clinic</span></p>
+							</div>
+
+						</div> <!--close .testimonial-one-->
+
+						<div class="testimonial-two indiv-testimonial">
+
+							<div>
+								<img src="wp-content/uploads/2013/03/maxi-milli-headshot.png" alt="maxi-milli-headshot" width="607" height="608" class="alignnone size-full wp-image-1459" />
+							</div>
+
+							<div class="testimonial-text">
+								<p>Do you like bread? Seriously, who doesn't? You have to check Le Red Bread's lovely loaves.
+									Fresh flavours every day...need I say more?</p>
+								<h3>Maxi Milli</h3>
+								<p>Chef - <span>Sailor Spoon</span></p>
+							</div>
+
+						</div> <!--close .testimonial-two-->
+
+						<div class="testimonial-three indiv-testimonial">
+
+							<div>
+								<img src="wp-content/uploads/2013/03/ana-vandana-headshot.png" alt="ana-vandana-headshot" width="607" height="607" class="alignnone size-full wp-image-1460" />
+							</div>
+
+							<div class="testimonial-text">
+								<p>Excellent cookies! They always have unique flavours and the cookies are always super fresh.
+									Make sure you get them before they sell out! And keep an eye out on holidays, the flavour combinations
+									they come up with are brilliant. Delicious!</p>
+								<h3>Anna Vandana</h3>
+								<p>Author - <span>Food Is Great Magazine</span></p>
+							</div>
+
+						</div> <!--close .testimonial-three-->
+
+						<div class="testimonial-four indiv-testimonial">
+
+							<div>
+								<img src="wp-content/uploads/2013/03/martha-m-masters-headshot.png" alt="martha-m-masters-headshot" width="607" height="608" class="alignnone size-full wp-image-1461" />
+							</div>
+
+							<div class="testimonial-text">
+								<p>Where has this bakery been all my life! I absolutely love their cookies and muffins.
+									Nom nom.</p>
+								<h3>Martha M. Masters</h3>
+								<p>Food Critic - <span>WikiTravel</span></p>
+							</div>
+
+						</div> <!--close .testimonial-four-->
+
+			</div> <!--close .testimonial-list-->
+		</section> <!--close .testimonials-section-->
 
 
 		</main><!-- #main -->
@@ -26,78 +106,3 @@ get_header(); ?>
 
 
 <?php get_footer(); ?>
-
-
-
-<!--
-
-
-<div class="testimonials-section">
-
-<div>
-
-<h2>What others say about us</h2>
-
-</div>
-
-<div class="testimonial-list">
-
-<div class="testimonial-one indiv-testimonial">
-
-<div>
-<img src="http://newsite.dev/wp-content/uploads/2013/03/dr-dosist-headshot.png" alt="dr-dosist-headshot" width="607" height="608" class="alignnone size-full wp-image-1458" />
-</div>
-
-<div class="testimonial-text">
-<p>Healthy food be damned! If you are a bakery and sweet addict like myself this place is life changing. So many goodies. Have a cheat day and eat all the pretzels!<p>
-<h3>Dr. Dosist</h3>
-<p>Nutritionist - <span>Health Time Clinic</span></p>
-</div>
-
-</div> <!--close .testimonial-one-->
-<!--
-<div class="testimonial-two indiv-testimonial">
-
-<div>
-<img src="http://newsite.dev/wp-content/uploads/2013/03/maxi-milli-headshot.png" alt="maxi-milli-headshot" width="607" height="608" class="alignnone size-full wp-image-1459" />
-</div>
-
-<div class="testimonial-text">
-<p>Do you like bread? Seriously, who doesn't? You have to check Le Red Bread's lovely loaves. Fresh flavours every day...need I say more?</p>
-<h3>Maxi Milli</h3>
-<p>Chef - <span>Sailor Spoon</span></p>
-</div>
-
-</div> <!--close .testimonial-two--> -->
-
-<!-- <div class="testimonial-three indiv-testimonial">
-
-<div>
-<img src="http://newsite.dev/wp-content/uploads/2013/03/ana-vandana-headshot.png" alt="ana-vandana-headshot" width="607" height="607" class="alignnone size-full wp-image-1460" />
-</div>
-
-<div class="testimonial-text">
-<p>Excellent cookies! They always have unique flavours and the cookies are always super fresh. Make sure you get them before they sell out! And keep an eye out on holidays, the flavour combinations they come up with are brilliant. Delicious!</p>
-<h3>Anna Vandana</h3>
-<p>Author - <span>Food Is Great Magazine</span></p>
-</div>
-
-</div> <!--close .testimonial-three-->
-
-<!-- <div class="testimonial-four indiv-testimonial">
-
-<div>
-<img src="http://newsite.dev/wp-content/uploads/2013/03/martha-m-masters-headshot.png" alt="martha-m-masters-headshot" width="607" height="608" class="alignnone size-full wp-image-1461" />
-</div>
-
-<div class="testimonial-text">
-<p>Where has this bakery been all my life! I absolutely love their cookies and muffins. Nom nom.</p>
-<h3>Martha M. Masters</h3>
-<p>Food Critic - <span>WikiTravel</span></p>
-</div> -->
-
-<!-- </div> <!--close .testimonial-four--> -->
-
-<!-- </div> <!--close .testimonial-list--> -->
-
-<!-- </div> <!--close .testimonials-section--> --> --> -->
