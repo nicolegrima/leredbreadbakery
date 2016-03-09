5f5c94ef63449720581b20a000b7fb7ba6069d7e
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-03-08 16:05:20 -0800

    Add About Page

---
+++ wp-content/themes/leredbread/page-about.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Template for About Page.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+      <div class="container">
+
+        <header class="entry-header about-header">
+              <h1 class="entry-title about-title">Learn About Our Team and Culture</h1>
+              <p>We are a team of creative and talented individuals who love making delicious treats.</p>
+        </header><!-- .entry-header -->
+
+      	<div class="entry-content about-content">
+
+          <div class="about-blocks">
+               <div class="about-block-item">
+                  <img src="wp-content/themes/leredbread/images/team.jpg" alt="Le Red Bread team">
+                  <h3>Le Red Bread Team</h3>
+                  <p class="sub-head">Baking up a storm every day.</p>
+                  <p>Le Red Bread’s staff is made up of an amazing team of inspired bakers.
+                    We really know our stuff when it comes to muffin tins and stand-up mixers.
+                    We also brew a mean cup of coffee.</p>
+               </div>
+
+               <div class="about-block-item">
+                  <img src="wp-content/themes/leredbread/images/bakery.jpg" alt="Le Red Bread bakery space">
+                  <h3>Le Red Bread Bakery</h3>
+                  <p class="sub-head">A home away from home.</p>
+                  <p>Our bakery in nestled away in a lovely little corner of Vancouver. Our state-of-the-art kitchen
+                    helps us serve only the best pretzels, loaves of bread, and sweet treats to our lovely customers.
+                    Pop in and say hi!</p>
+               </div>
+            </div>
+
+            <h2 class="about-subtitle">Our Story</h2>
+
+            <p>Le Red Bread has been Vancouver baked-good icon for more than two months!
+              Customers often comment on the bustle of activity they see in the bakery behind the cafe…
+              that’s where the magic happens every day.</p>
+
+            <p>Our day begins at the crack of dawn and we start baking almost immediately.
+              Cookie and muffin batters that were prepared the night before are poured into tins
+              and popped in the ovens, bread dough rises and gets popped into the oven, and pretzels are crossed.</p>
+
+            <p>By breakfast time we’re open for business and greeting our first customers,
+              pouring coffees as we serve delectable treats.</p>
+
+            <p>We’re committed to continuing the Le Red Bread tradition in Vancouver and we still
+              produce our fabulous range according to the original recipes passed down from generations of
+              master bread artisans.</p>
+
+              <section class="suggestions">
+                <p>
+                   <span>Feel free to contact us with any questions coments or suggestions.
+                     We even take custom orders!</span>
+                   <a href="/contact/" class="btn">Contact Us</a>
+                </p>
+              </section>
+
+        </div><!-- .entry-content -->
+
+      </div><!-- .container -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
