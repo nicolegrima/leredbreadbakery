362cbe4cf8045eab0dcae6fd1c7b75f6881e1d1a
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-03-13 16:00:35 -0700

    Adjust all CSS to SASS

--- wp-content/themes/leredbread/template-parts/product-icons.php
+++ wp-content/themes/leredbread/template-parts/product-icons.php
@@ -16,7 +16,7 @@
 
    <?php foreach( $terms as $term) : ?>
 
-     <div class="product-category-single">
+     <div class="product-title-single">
 
        <img src="<?php echo get_template_directory_uri() . '/images\/' . $term->slug; ?>.png" alt ="" />
        <a href="<?php echo get_term_link( $term ); ?>"><h3><?php echo $term->name; ?></h3></a>
